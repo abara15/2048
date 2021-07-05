addefb9963a17428209e50898f7307519e7a103f
abara15 <61340421+abara15@users.noreply.github.com>
2021-07-05 15:29:44 +1000

    style: added colours to the blocks

---
+++ 2048/src/movements.js
@@ -0,0 +1,169 @@
+import cloneDeep from 'lodash.clonedeep';
+import { addNumber } from './util';
+
+export const swipeLeft = (data, setData) => {
+  let oldGrid = data;
+  let newArray = cloneDeep(data);
+
+  console.table(newArray);
+  for (let i = 0; i < 4; i++) {
+    let b = newArray[i];
+    let slow = 0;
+    let fast = 1;
+    while (slow < 4) {
+      if (fast === 4) {
+        fast = slow + 1;
+        slow++;
+        continue;
+      }
+      if (b[slow] === 0 && b[fast] === 0) {
+        fast++;
+      } else if (b[slow] === 0 && b[fast] !== 0) {
+        b[slow] = b[fast];
+        b[fast] = 0;
+        fast++;
+      } else if (b[slow] !== 0 && b[fast] === 0) {
+        fast++;
+      } else if (b[slow] !== 0 && b[fast] !== 0) {
+        if (b[slow] === b[fast]) {
+          b[slow] = b[slow] + b[fast];
+          b[fast] = 0;
+          fast = slow + 1;
+          slow++;
+        } else {
+          slow++;
+          fast = slow + 1;
+        }
+      }
+    }
+  }
+  if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+    addNumber(newArray);
+  }
+
+  setData(newArray);
+}
+
+export const swipeRight = (data, setData) => {
+  let oldData = data;
+  let newArray = cloneDeep(data);
+
+  for (let i = 3; i >= 0; i--) {
+    let b = newArray[i];
+    let slow = b.length - 1;
+    let fast = slow - 1;
+    while (slow > 0) {
+      if (fast === -1) {
+        fast = slow - 1;
+        slow--;
+        continue;
+      }
+      if (b[slow] === 0 && b[fast] === 0) {
+        fast--;
+      } else if (b[slow] === 0 && b[fast] !== 0) {
+        b[slow] = b[fast];
+        b[fast] = 0;
+        fast--;
+      } else if (b[slow] !== 0 && b[fast] === 0) {
+        fast--;
+      } else if (b[slow] !== 0 && b[fast] !== 0) {
+        if (b[slow] === b[fast]) {
+          b[slow] = b[slow] + b[fast];
+          b[fast] = 0;
+          fast = slow - 1;
+          slow--;
+        } else {
+          slow--;
+          fast = slow - 1;
+        }
+      }
+    }
+  }
+  if (JSON.stringify(oldData) !== JSON.stringify(newArray)) {
+    addNumber(newArray);
+  }
+
+  setData(newArray);
+}
+
+export const swipeUp = (data, setData) => {
+  let b = [...data];
+  let oldData = JSON.parse(JSON.stringify(data));
+  for (let i = 0; i < 4; i++) {
+    let slow = 0;
+    let fast = 1;
+    while (slow < 4) {
+      if (fast === 4) {
+        fast = slow + 1;
+        slow++;
+        continue;
+      }
+      if (b[slow][i] === 0 && b[fast][i] === 0) {
+        fast++;
+      } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+        b[slow][i] = b[fast][i];
+        b[fast][i] = 0;
+        fast++;
+      } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+        fast++;
+      } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+        if (b[slow][i] === b[fast][i]) {
+          b[slow][i] = b[slow][i] + b[fast][i];
+          b[fast][i] = 0;
+          fast = slow + 1;
+          slow++;
+        } else {
+          slow++;
+          fast = slow + 1;
+        }
+      }
+    }
+  }
+  if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+    addNumber(b);
+  }
+
+  setData(b);
+}
+
+export const swipeDown = (data, setData) => {
+  console.log(data);
+  let b = [...data];
+  let oldData = JSON.parse(JSON.stringify(data));
+  for (let i = 3; i >= 0; i--) {
+    let slow = b.length - 1;
+    let fast = slow - 1;
+    while (slow > 0) {
+      if (fast === -1) {
+        fast = slow - 1;
+        slow--;
+        continue;
+      }
+      if (b[slow][i] === 0 && b[fast][i] === 0) {
+        fast--;
+      } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+        b[slow][i] = b[fast][i];
+        b[fast][i] = 0;
+        fast--;
+      } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+        fast--;
+      } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+        if (b[slow][i] === b[fast][i]) {
+          b[slow][i] = b[slow][i] + b[fast][i];
+          b[fast][i] = 0;
+          fast = slow - 1;
+          slow--;
+        } else {
+          slow--;
+          fast = slow - 1;
+        }
+      }
+    }
+  }
+
+  if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+    addNumber(b);
+  }
+
+  setData(b);
+}
---
+++ 2048/src/util.js
@@ -0,0 +1,64 @@
+import { useEffect } from "react"
+
+export const useEvent = (event, handler, passive = false) => {
+    useEffect(() => {
+        // initiate the event handler
+        window.addEventListener(event, handler, passive);
+
+        // this will cleanup the event every time the component unmounts
+        return function cleanup () {
+            window.removeEventListener(event, handler);
+        }
+    })
+}
+
+// AddNumber - add an item
+export const addNumber = (newGrid) => {
+  let added = false;
+  let gridFull = false;
+  let attempts = 0;
+  while (!added) {
+    if (gridFull) {
+      break;
+    }
+    
+    // Find two random numbers for grid indexes (0-3)
+    let rand1 = Math.floor(Math.random() * 4);
+    let rand2 = Math.floor(Math.random() * 4);
+    // Increment attempts
+    attempts++;
+    if (newGrid[rand1][rand2] === 0) {
+      newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+      added = true;
+    }
+  }
+}
+
+export const getColors = (number) => {
+  switch (number) {
+    case 2:
+      return "#EBDCD0";
+    case 4:
+      return "#E9DBBA";
+    case 8:
+      return "#E9A067";
+    case 16:
+      return "#F08151";
+    case 32:
+      return "#F2654F";
+    case 64:
+      return "#F1462C";
+    case 128:
+      return "#E7C65E";
+    case 256:
+      return "#E8C350";
+    case 512:
+      return "#E8BE40";
+    case 1024:
+      return "#E8BB31";
+    case 2048:
+      return "#E7B723";
+    default:
+      return "#C2B3A3";
+  }
+}
