e539681df148fafab1a9fd02a9e7eb4ee8297b92
abara15 <61340421+abara15@users.noreply.github.com>
2021-07-05 12:04:26 +1000

    layout: completed main layout of grid and some general styling

--- 2048/src/App.js
+++ 2048/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	const [data, setData] = useState([
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+	]);
+
+  // Initialise
+
+  // AddNumber - add an item
+
+  // Swipe - right, left, up, down
+
+  // Check GameOver
+
+  // Reset
+
+	return (
+		<div
+      style={style.main}
+    >
+      {data.map((row, index) => {
+        return (
+          <div style={{ display: 'flex' }} key={index}>
+            {row.map((digit, i) => (
+              <Block number={digit} key={i} />
+            ))}
+          </div>
+        );
+      })}
+		</div>
+	);
+}
+
+const Block = ({number}) => {
+	return (
+    <div
+      style={style.block}
+    >
+      {number}
     </div>
   );
+}
+
+const style = {
+  main: {
+    background: "#AD9D8F",
+    width: "max-content",
+    margin: "auto",
+    padding: 5,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  block: {
+    height: 80,
+    width: 80,
+    background: "lightgray",
+    margin: 3,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 45,
+    fontWeight: 800,
+    color: "white",
+  },
 }
 
 export default App;
