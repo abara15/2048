c34714f2fc1c526307daafca9b3138d3b75ba54f
abara15 <61340421+abara15@users.noreply.github.com>
2021-07-05 14:58:41 +1000

    functions: wrote functions to control movements and addition

--- 2048/src/App.js
+++ 2048/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import cloneDeep from 'lodash.clonedeep';
 
 function App() {
 	const [data, setData] = useState([
@@ -9,15 +10,215 @@ function App() {
 		[0, 0, 0, 0],
 	]);
 
-  // Initialise
+  // initialize
+  const initialize = () => {
+    let newGrid = cloneDeep(data);
+    console.log(newGrid);
+
+    addNumber(newGrid);
+    console.table(newGrid);
+    addNumber(newGrid);
+    console.table(newGrid);
+    setData(newGrid);
+  }
 
   // AddNumber - add an item
+  const addNumber = (newGrid) => {
+    let added = false;
+    let gridFull = false;
+    let attempts = 0;
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+      
+      // Find two random numbers for grid indexes (0-3)
+      let rand1 = Math.floor(Math.random() * 4);
+      let rand2 = Math.floor(Math.random() * 4);
+      // Increment attempts
+      attempts++;
+      if (newGrid[rand1][rand2] === 0) {
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+      }
+    }
+  }
 
   // Swipe - right, left, up, down
+  const swipeLeft = () => {
+    let oldGrid = data;
+    let newArray = cloneDeep(data);
+
+    console.table(newArray);
+    for (let i = 0; i < 4; i++) {
+      let b = newArray[i];
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast];
+          b[fast] = 0;
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast];
+            b[fast] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+      addNumber(newArray);
+    }
+
+    setData(newArray);
+  }
+  const swipeRight = () => {
+    let oldData = data;
+    let newArray = cloneDeep(data);
+
+    for (let i = 3; i >= 0; i--) {
+      let b = newArray[i];
+      let slow = b.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast--;
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast];
+          b[fast] = 0;
+          fast--;
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast--;
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast];
+            b[fast] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(newArray)) {
+      addNumber(newArray);
+    }
+
+    setData(newArray);
+  }
+  const swipeUp = () => {
+    let b = [...data];
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < 4; i++) {
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast++;
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i];
+          b[fast][i] = 0;
+          fast++;
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast++;
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i];
+            b[fast][i] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+      addNumber(b);
+    }
+
+    setData(b);
+  }
+  const swipeDown = () => {
+    console.log(data);
+    let b = [...data];
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 3; i >= 0; i--) {
+      let slow = b.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast--;
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i];
+          b[fast][i] = 0;
+          fast--;
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast--;
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i];
+            b[fast][i] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+
+    if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+      addNumber(b);
+    }
+
+    setData(b);
+  }
 
   // Check GameOver
 
   // Reset
+  
+
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
 
 	return (
 		<div
@@ -64,7 +265,7 @@ const style = {
     justifyContent: "center",
     alignItems: "center",
     fontSize: 45,
-    fontWeight: 800,
+    fontWeight: 400,
     color: "white",
   },
 }
